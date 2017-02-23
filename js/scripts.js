b2ff86f43b0454de0bd6d0fe0e0682fe3251e1bf
Chris Carr and Riley Watts <riley@>
2017-02-23 15:37:41 -0800

    finalize js functionality, organize js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business Logic
 var romNum = function(input) {
   var I = 1;
   var V = 5;
@@ -28,136 +29,52 @@ var romNum = function(input) {
         whole = Math.floor((remainder/numeralValues[i]));
         pushTo.push(whole);
         remainder = remainder%numeralValues[i];
-        console.log(whole);
       }
     }
   }
 
-console.log(pushTo);
-
-// debugger;
-// for(var i = pushTo.length - 1; i >= 0; i--)
-
-for(var i = 0; i < pushTo.length; i++) {
-  if(pushTo[i]===4) {
-    if(i === 6) {
-      if(pushTo[i-1] === 1) {
-
-        numString += "IX";
-      } else {
-        console.log(4);
-        numString += "IV";
+  for(var i = pushTo.length - 1; i >= 0; i--) {
+    if(pushTo[i]===4) {
+      if(i === 6) {
+        if(pushTo[i-1] === 1) {
+          pushTo[5]=0;
+          numString.unshift("IX");
+        } else {
+          numString.unshift("IV");
+        }
+      } else if (i === 4) {
+        if(pushTo[i-1] === 1) {
+          pushTo[3]=0;
+          numString.unshift("XC");
+        } else {
+          numString.unshift("XL");
+        }
+      } else if (i === 2) {
+        if(pushTo[i-1] === 1) {
+          pushTo[1]=0;
+          numString.unshift("CM");
+        } else {
+          numString.unshift("CD")
+        }
       }
-    } else if (i === 4) {
-      if(pushTo[i-1] === 1) {
-        numString += "XC";
-        console.log(90);
-      } else {
-        console.log(40);
-        numString+= "XL";
-      }
-    } else if (i === 2) {
-      if(pushTo[i-1] === 1) {
-        numString += "CM";
-        console.log(900);
-      } else {
-        numString+= "CD"
-        console.log(400);
-      }
-    }
-
-  } else {
-    for(var j = 1; j <= pushTo[i]; j++) {
-
-      numString += numeralStrings[i];
-
+    } else {
+        for(var j = 1; j <= pushTo[i]; j++) {
+          numString.unshift(numeralStrings[i]);
+        }
     }
   }
-  // console.log(
+
+  return numString.join("");
 }
 
-console.log(numString);
-
-// (pushTo[2]===4) || (pushTo[4]===4) || (pushTo[6]===4)
-
-
-// Works
-  // if (input % M !== 0) {
-  //   var newM = input/M;
-  //   newM = parseInt(newM.toFixed(0));
-  //   var remainder = (input % M);
-  //   console.log(newM, remainder);
-  // }
-  // else {
-  //   alert("perfect");
-  // }
-
-// var numerals = [M,D,C,L,X,V,I];
-// var remainder = input;
-// var pushTo = [];
-// debugger;
-// for(var i=0; i < numerals.length; i++) {
-//   var whole = 0;
-//   if(remainder%numerals[i] === 0) {
-//     whole = Math.floor(remainder/numerals[i]);
-//     pushTo.push(whole);
-//   }
-//   else {
-//     if(remainder < numerals[i]) {
-//       pushTo.push(0);
-//     }
-//     else {
-//       whole = Math.floor((remainder/numerals[i]));
-//       pushTo.push(whole);
-//       remainder = remainder%numerals[i];
-//       console.log(whole);
-//     }
-//   }
-// }
-//
-// console.log(pushTo);
-
-
-  //  else if (input === D) {
-  //   return "D";
-  // } else if (input === C) {
-  //   return "C";
-  // } else if (input === L) {
-  //   return "L";
-  // } else if (input === X) {
-  //   return "X";
-  // } else if (input % V !== 0) {
-  //   var newV = input/M;
-  //   newV = parseInt(newV.toFixed(0));
-  //   var remainder = (input % V);
-  //   console.log(newV, remainder);
-  // } else if (input === I) {
-  //   return "I";
-  // }
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
+// User Interface
 $(function() {
   $("#formOne").submit(function(event) {
+    event.preventDefault();
 
     var input = parseInt($("#input").val());
-    // console.log(I,V,X,L,C,D,M);
-    // $("#output").append(numString);
-    console.log(romNum(input));
+    var output = romNum(input);
 
-    console.log(input);
-    event.preventDefault();
+    $("#output").append(output);
   });
 });
